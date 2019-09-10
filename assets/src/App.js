81a537aa323f94b1c83236fabd4b559298b07469
Pavel Tsiukhtsiayeu <paveltyk@gmail.com>
2019-09-10 18:41:28 +0300

    React app

--- assets/src/App.js
+++ assets/src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState('');
+  const [isSubmitted, setSubmitted] = useState(false);
+  const [isProcessing, setProcessing] = useState(false);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    try {
+      setProcessing(true);
+      await axios.post('/api/subscriptions', { email });
+      setSubmitted(true);
+    } catch (error) {
+      setSubmitted(true);
+      setError('server responded with error');
+    }
+  }
+
+  if (isSubmitted) {
+    return (
+      <div className="App">
+        {
+          error
+            ? <p className="error">Failed to submit: {error}</p>
+            : <p className="success">Thank you! Email <b>"{email}"</b> has been added to the list.</p>
+        }
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Your email..."
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          disabled={isProcessing} />
+      </form>
     </div>
   );
 }
